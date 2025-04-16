8cc67e9ea3dc666133db344d34f60e6e9936c7c0
iOmega17 <mr.anujps@gmail.com>
2025-04-16 09:27:44 +0000

    Written the Authentication parts 1604-1457

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -1,3 +1,12 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+using dotnetapp.Data;
+using dotnetapp.Services;
+using Microsoft.AspNetCore.Identity;
+using dotnetapp.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,12 +14,44 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 
+//Configuring entity framework core for Sql Server
+builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("connstring")));
+
+//Services Dependency
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<BookService>();
+
+//Configuring Identity
+builder.Services.AddIdenttiy<ApplicationUser, IdentityRole>()
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+//Configuring JWT Authentication
+var jwtSetting = builder.Configuration.GetSection("JWT");
+var secretKey = Encoding.UTF8.getBytes(jwtSettings["Secret"]);
+
+builder.Services.AffAuthentication(options=>{
+    options.DefaultAuthenticationScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options=>{
+    options.TokenValidationParameters = new TokenValidationParameters{
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = jwtSettings["ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecuritykey(sercetKey)
+
+    };
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+//Adding Authorization
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -25,6 +66,7 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseAuthentication();
 app.MapControllers();
 
 app.Run();
