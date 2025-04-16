c70a0a8df555ed24c4cfb1a2c87c4b913825d4b1
Ujwalasony <vangaujwalasony@gmail.com>
2025-04-16 09:29:07 +0000

    Changes in BookService and ApplicationDbContext 1604:14.58

---
+++ dotnetapp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnetapp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace dotnetapp.Data
+{
+    public class ApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+         public DbSet<User> Users { get; set; }
+         public DbSet<Book> Books { get; set; }
+        }
+    }
+}
--- dotnetapp/Services/BookService.cs
+++ dotnetapp/Services/BookService.cs
@@ -2,11 +2,75 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using dotnetapp.Data;
+using dotnetapp.Models;
 
 namespace dotnetapp.Services
 {
     public class BookService
     {
+        private readonly ApplicationDbContext _context;
+        public BookService(ApplicationDbContext context)
+        {
+            _context=context;
+        }
+        public async Task<bool> AddBook(Book book)
+        {
+            if (_context.Books.Any(b => b.Title == book.Title))
+            {
+                throw new Exception("Failed to add book");
+            }
+ 
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+ 
+       public async Task<Book> GetBookById(int bookId)
+        {
+            var book = await _context.Books.FindAsync(bookId);
+            if (book == null)
+            {
+                throw new Exception("Cannot find any book");
+            }
+            return book;
+        }
+        public async Task <IEnumerable<Book>> GetAllBooks()
+        {
+            return await _context.ToListAsync();
+        }
         
+        public async Task<bool> UpdateBook(int bookId,Book book)
+        {
+               var books= await _context.Books.FindAsync(bookId);
+               if(books==null)
+               {
+                return false;
+               }
+                books.BookId=book.BookId;
+                books.Title=book.Title;
+                books.Author=book.Author;
+                books.Genre=book.Genre;
+                books.PublishedDate=book.PublishedDate;
+                books.CoverImage=book.CoverImage;
+                await _context.SaveChangesAsync();
+                return true;
+        }
+        public async Task<book> DeleteBook(int bookId)
+        {
+            var books=await _context.FindAsync(bookId);
+            if(books==null)
+            {
+                return false;
+            }
+            Books.Remove(bookId);
+            await _context.SaveChangesAsync();
+            return true;
+        }
     }
 }
+
+
+ 
