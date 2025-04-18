9ab54d8d21bc8955ba25a569444d8c67bcb670ae
druvareddy25 <druvareddy32@gmail.com>
2025-04-18 13:19:23 +0000

    bookrecomender and homepage done

--- reactapp/src/Components/HomePage.jsx
+++ reactapp/src/Components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BookRecommenderNavbar from '../BookReaderComponents/BookReaderNavbar';
+import BookRecommenderNavbar from '../BookRecommenderComponents/BookRecommenderNavbar';
 
 
 const HomePage = () => {
@@ -9,16 +9,18 @@ const HomePage = () => {
       <BookRecommenderNavbar/>
       <div className="container text-center">
         <div className="position-relative mb-4">
-          <img src='./bookfindercoverimage.jpeg' alt="BookFinder" className="img-fluid rounded" />
-          <h1 className="position-absolute top-50 start-50 translate-middle text-white">BookFinder</h1>
+        <img src='./bookfindercoverimage.jpeg' alt="BookFinder" width="500" height="600" className="img-fluid rounded" />
+          <h1 className="position-absolute top-50 start-50 translate-middle w-100 bg-white text-dark">BookFinder</h1>
         </div>
-        <p className="lead">An app to discover, explore, and recommend books tailored to your reading preferences.</p>
-        <div>
+        <div className='w-60 bg-white text-dark'>
+        <span className="lead ">An app to discover, explore, and recommend books tailored to your reading preferences.</span>
+        </div>
+        <div className='w-100 text-center'>
           
-        <footer className="bg-dark text-white p-3 mt-4 text-center">
-          <p className="font-weight-bold">Contact Us</p>
-          <p>Email: <a href="mailto:telos@support.com" className="text-white">telos@support.com</a></p>
-          <p>Phone: 007</p>
+        <footer className="bg-dark text-white p-3 mt-4 text-center d-flex flex-column">
+          <span className="font-weight-bold ">Contact Us</span>
+          <span>Email: <a href="mailto:telos@support.com" className="text-white">telos@support.com</a></span>
+          <span>Phone: 007</span>
         </footer>
         </div>
       </div>
