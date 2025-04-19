b37e5c30dbe10fdd8875557c02333ddb1204511b
druvareddy25 <druvareddy32@gmail.com>
2025-04-19 07:36:11 +0000

    homepage css done

--- reactapp/src/Components/HomePage.jsx
+++ reactapp/src/Components/HomePage.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BookRecommenderNavbar from '../BookRecommenderComponents/BookRecommenderNavbar';
-
+import './HomePage.css';
 
 const HomePage = () => {
   return (
     <div>
-      <BookRecommenderNavbar/>
+      <BookRecommenderNavbar/><br/>
       <div className="container text-center">
-        <div className="position-relative mb-4">
-        <img src='./bookfindercoverimage.jpeg' alt="BookFinder" width="500" height="600" className="img-fluid rounded" />
-          <h1 className="position-absolute top-50 start-50 translate-middle w-100 bg-white text-dark">BookFinder</h1>
-        </div>
-        <div className='w-60 bg-white text-dark'>
-        <span className="lead ">An app to discover, explore, and recommend books tailored to your reading preferences.</span>
-        </div>
-        <div className='w-100 text-center'>
-          
-        <footer className="bg-dark text-white p-3 mt-4 text-center d-flex flex-column">
-          <span className="font-weight-bold ">Contact Us</span>
-          <span>Email: <a href="mailto:telos@support.com" className="text-white">telos@support.com</a></span>
-          <span>Phone: 007</span>
-        </footer>
+        <div className="position-relative mb-4" style={{ 
+          backgroundImage: "url('/bookbgimage.jpg')", 
+          backgroundSize: 'cover', 
+          backgroundPosition: 'center', 
+          minHeight: '50vh' 
+        }}>
+          <div>
+            <img src='./bookimage.jpg' alt="BookFinder" width="500" height="700" className="img-fluid rounded" />
+            <h1 className="blur-background ">BookFinder</h1>
+          </div><br/>
+          <div className='w-60 bg-white text-dark'>
+            <span className="w-50 lead">An app to discover, explore, and recommend books tailored to your reading preferences.</span>
+          </div>
+          <div className='w-100 text-center'>
+            <footer className="bg-dark text-white p-3 mt-4 text-center d-flex flex-column">
+              <span className="font-weight-bold">Contact Us</span>
+              <span>Email: <a href="mailto:telos@support.com" className="text-white">telos@support.com</a></span>
+              <span>Phone: 007</span>
+            </footer>
+          </div>
         </div>
       </div>
     </div>
